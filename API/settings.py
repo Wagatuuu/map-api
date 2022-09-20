738ecb9dffa6c145ca2c308484e1ff6025ec9925
Sceptre <mbuguag026@gmail.com>
2022-09-20 15:57:03 +0300

    deployment

--- API/settings.py
+++ API/settings.py
@@ -157,20 +157,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Geodjango SETUP
 
-if os.name == 'nt':
-    import platform
-    OSGEO4W = r"C:\OSGeo4W"
-    if '64' in platform.architecture()[0]:
-        OSGEO4W += "64"
-    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
-    os.environ['OSGEO4W_ROOT'] = OSGEO4W
-    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
-    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
-    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
+# if os.name == 'nt':
+#     import platform
+#     OSGEO4W = r"C:\OSGeo4W"
+#     if '64' in platform.architecture()[0]:
+#         OSGEO4W += "64"
+#     assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
+#     os.environ['OSGEO4W_ROOT'] = OSGEO4W
+#     os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+#     os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+#     os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
 
-OSGEO_VENV = Path(__file__).parents[1] / 'venv/Lib/site-packages/osgeo/'
-GEOS_LIBRARY_PATH = str(OSGEO_VENV / 'geos_c.dll')
-GDAL_LIBRARY_PATH = str(OSGEO_VENV / 'gdal305.dll')
+# GDAL_LIBRARY_PATH = '/home/sue/local/lib/libgdal.so'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
