7eb2b3a1fb334a2ac4e2b6a075cafcf9aec115c7
MbuguaGeorge <mbuguageorge026@gmail.com>
2023-02-17 01:50:08 +0300

    initial commit

--- API/settings.py
+++ API/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
