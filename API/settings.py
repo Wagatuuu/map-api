1d842bab84e9bb784c2ee7fdbc85f5dba812289b
MbuguaGeorge <mbuguageorge026@gmail.com>
2023-02-17 00:25:04 +0300

    initial commit

--- API/settings.py
+++ API/settings.py
@@ -165,7 +165,5 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
     'https://noise-screening-lite.netlify.app',
 ]
