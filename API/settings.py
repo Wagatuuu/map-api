88aa1233f9e3e50408f81a8ee524dc2d94b42471
MbuguaGeorge <mbuguageorge026@gmail.com>
2023-02-20 01:32:24 +0300

    initial

--- API/settings.py
+++ API/settings.py
@@ -89,12 +89,8 @@ AUTH_USER_MODEL = 'converter.UserProfile'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'map1',
-        'USER':'postgres',
-        'PASSWORD':'George026#',
-        'HOST':'127.0.0.1',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
